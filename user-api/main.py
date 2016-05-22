3a94172cc0b492c9a1bd77e4e817ea14cdceb1ee
jsanguinetti <jsangui@gmail.com>
2016-05-22 19:49:45 -0300

    added error messages

--- user-api/main.py
+++ user-api/main.py
@@ -1,6 +1,7 @@
 from users import users
 from flask import Flask
 from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
 
@@ -10,6 +11,20 @@ app.register_blueprint(users)
 @app.route('/')
 def hello_world():
     return jsonify({"message": "Hello World!"})
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    resp = jsonify({'message': request.url + ' not found'})
+    resp.status_code = 404
+    return resp
+
+
+@app.errorhandler(500)
+def unknown_error(error=None):
+    resp = jsonify({'message': error.message})
+    resp.status_code = 500
+    return resp
 
 
 if __name__ == '__main__':
