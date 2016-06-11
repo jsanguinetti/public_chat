d9be2bf878f9a445e0da4c801301551e94321e95
jsanguinetti <jsangui@gmail.com>
2016-06-11 18:00:43 -0300

    added full text search through search api

--- messages.py
+++ messages.py
@@ -1,10 +1,18 @@
-from flask import Blueprint, jsonify, request, json
+from flask import Blueprint, jsonify, request, json, render_template
 from models.chat import chat_key_from_name
 from models.message import Message
 from google.appengine.api import memcache
+from google.appengine.api import search
 
+_INDEX_NAME = 'messages'
 
 messages = Blueprint('messages', __name__)
+
+
+def create_document(message_dict):
+    return search.Document(
+        fields=[search.TextField(name='user', value=message_dict['user']['nickname']),
+                search.TextField(name='content', value=message_dict['content'])])
 
 
 @messages.route('/chats/<chat_name>/messages', methods=['POST'])
@@ -13,6 +21,15 @@ def message_post(chat_name):
                       content=request.get_json()['content'])
     message.put()
     memcache.incr('message_count')
-    # must handle json response in javascript.
-    # important because we will use pusher
-    return jsonify(message.to_dict())
+    message_dict = message.to_dict()
+    search.Index(name=_INDEX_NAME).put(create_document(message_dict))
+    return jsonify(message_dict)
+
+
+@messages.route('/messages', methods=['GET'])
+def message_search():
+    query = request.args.get('search')
+    query_obj = search.Query(query_string=query)
+    results = search.Index(name=_INDEX_NAME).search(query=query_obj)
+    return render_template('message_result_page.html',
+                           results=results)
