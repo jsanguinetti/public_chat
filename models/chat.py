da0f4512169344be6da56c67200ec2ce8bf3f68e
jsanguinetti <jsangui@gmail.com>
2016-06-16 23:30:38 -0300

    sending correct email with appengine api

--- models/chat.py
+++ models/chat.py
@@ -1,4 +1,5 @@
 from google.appengine.ext import ndb
+from google.appengine.api import mail
 
 
 class Chat(ndb.Model):
@@ -7,6 +8,23 @@ class Chat(ndb.Model):
 
     def url_name(self):
         return self.name.replace(" ", "%20")
+
+    def send_congratulations_email_to(self, current_user):
+        message = mail.EmailMessage(
+            sender="no-reply@apps-escalables-public-chat.appspotmail.com",
+            subject="Your chat '" + self.name + "' was created")
+        message.to = (current_user.nickname() + " " + "<" + current_user.email() + ">")
+        message.body = """Dear %s:
+        Your chat '%s' has been created.  You can now visit
+        https://apps-escalables-public-chat.appspot.com/chats/%s and sign in using your Google Account to
+        access it.
+
+        Please let us know if you have any questions.
+
+        The PublicChat Team
+        """ % (current_user.nickname(), self.name, self.url_name())
+        message.send()
+        pass
 
 
 def chat_key_from_name(chat_name):
